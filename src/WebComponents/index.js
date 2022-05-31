64421bd860fd118b5fa086e1cc60ec385cfeff95
Michal Jeznach <michal6h@wp.pl>
2022-05-31 15:12:34 +0200

    creata basic template

--- src/WebComponents/index.js
+++ src/WebComponents/index.js
@@ -1,30 +1,111 @@
 const template = document.createElement("template");
 
 template.innerHTML = `
-<link href="/src/css/main.css" rel="stylesheet">
+<style>
+.container { 
+    position: relative;
+width: 1440px;
+height: 960px;
+
+background: #FFFFFF;
+        
+    }
+img.responsive {
+    position: absolute;
+    width: 505px;
+    height: 330px;
+    left: 230px;
+    top: 96px;
+    
+}
+.global {
+    position: absolute;
+width: 380px;
+height: 250px;
+left: 830px;
+top: 136px;
+}
+.title {
+    
+width: 380px;
+height: 50px;
+left: calc(50% - 380px/2 + 300px);
+top: calc(50% - 50px/2 - 319px);
+font-family: 'Roboto';
+font-style: normal;
+font-weight: 700;
+font-size: 42px;
+line-height: 120%;
+color: #121212;
+}
+.description {
+   
+width: 380px;
+height: 96px;
+left: calc(50% - 380px/2 + 300px);
+top: calc(50% - 96px/2 - 222px);
+font-family: 'Roboto';
+font-style: normal;
+font-weight: 300;
+font-size: 16px;
+line-height: 150%;
+color: #121212;
+}
+.buttonstyle {
+    
+width: 128px;
+height: 48px;
+left: 830px;
+top: 338px;
+
+}
+.button{
+display: flex;
+flex-direction: row;
+align-items: flex-start;
+padding: 16px 40px;
+gap: 10px;
+background: #E4C1AE;
+border-radius: 32px;
+border-color: white;
+}
+</style>
+
 <div class="container">
- <div class="picture">
- <h1> Hello <h1/>
- <img />
+ <img class="responsive"/>
+ <div class="global">
+ <div class="title">Lorem Ipsum</div>
+ <div class="description">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley</div>
+ <div class="buttonstyle">
+ <button class="button">button</button>
  </div>
 
 
+ </div>
+ 
  </div>`;
 
 
 class WebComponent extends HTMLElement {
 constructor() {
 super();
+const shadowRoot = this.attachShadow({ mode: 'open' });
+shadowRoot.appendChild(template.content);
 
 }
-connectedCallback() {
-     const shadowRoot = this.attachShadow({ mode: 'open' });
-    shadowRoot.appendChild(template.content);
 
+static get observedAttributes() {
+    return ["avatar"];
+  }
 
+attributeChangedCallback(name, oldValue, newValue) {
+    this.shadowRoot.querySelector("img.responsive").src=
+    this.getAttribute("avatar")
 }
+
+
 }
 
 window.customElements.define('web-component', WebComponent);
 
-// export default WebComponent;
