4c3d0bd9a632d07d16eb1f79d246c04d35e2848a
Michal Jeznach <michal6h@wp.pl>
2022-06-07 23:09:16 +0200

    finished modal/ create button in modal/

--- src/WebComponents/index.js
+++ src/WebComponents/index.js
@@ -6,6 +6,7 @@ template.innerHTML = `
 @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap');
 
 .container { 
+display: flex;
 position: relative;
 width: 1440px;
 height: 100%;
@@ -13,6 +14,7 @@ z-index: -1;
 }
 
 img.responsive {
+display: flex;
 position: absolute;
 width: 505px;
 height: 330px;
@@ -36,7 +38,7 @@ left: calc(50% - 380px/2 + 300px);
 top: calc(50% - 50px/2 - 319px);
 }
 
-.titletext {
+.title-text {
  
 font-family:'Roboto';
 font-style: normal;
@@ -104,6 +106,11 @@ z-index: 3;
   
 }
 .header-text {
+position: absolute;
+width: 380px;
+height: 50px;
+left: calc(50% - 380px/2 - 175px);
+top: 24px;
 font-family: 'Roboto';
 font-style: normal;
 font-weight: 700;
@@ -131,15 +138,40 @@ z-index: 2;
 
 
 }
+.info-modal{
+position: absolute;
+width: 380px;
+height: 24px;
+left: calc(50% - 380px/2 - 175px);
+top: calc(50% - 24px/2 + 37px);
+font-family: 'Roboto';
+font-style: normal;
+font-weight: "blend";
+font-size: 16px;
+line-height: 150%;
+}
 
+.counter-text {
+font-weight: bolder;
+}
 
+#clear_button {
+display: none;
+position: absolute;
+Width: 60px;
+Height: 60px;
+Top: 120px;
+right: 20px;
+font-size: 15px;
+color: black;
+}
   </style>
   
   <div class="container">
     <img class="responsive"/>
       <div class="global">
         <div class="title">
-          <h1 class="titletext"></h1>
+          <h1 class="title-text"></h1>
         </div>
         <div class="description">
           <p class="description-text"></p>
@@ -154,7 +186,8 @@ z-index: 2;
     <div class="modal-header">
       <span class="close">&times;</span>
       <p class="header-text">Alert!</p>
-      <span id="counter" class="counter-text">0</span>
+      <p class="info-modal">You have clicked <span id="count" class="counter-text"></span> times to related buttons</p>
+       <button id="clear_button">Clear</button>
     </div>
     <div class="modal-body">
         <slot><slot>
@@ -168,7 +201,7 @@ z-index: 2;
 class WebComponent extends HTMLElement {
   constructor() {
     super();
-
+    
     const shadowRoot = this.attachShadow({ mode: "open" });
     shadowRoot.appendChild(template.content);
     
@@ -178,35 +211,49 @@ class WebComponent extends HTMLElement {
     return ["avatar", "name", "text"];
   }
   connectedCallback() {
+    
     this._modal = this.shadowRoot.querySelector(".modal");
     this._overlay = this.shadowRoot.querySelector(".overlay");
-    // this._counter = this.shadowRoot.querySelector(".increment")
+    this._clear_button = this.shadowRoot.querySelector('#clear_button');
     this.shadowRoot.querySelector("button").addEventListener('click',this._showModal.bind(this));
     this.shadowRoot.querySelector(".close").addEventListener('click',this._hideModal.bind(this));
     this.shadowRoot.querySelector(".overlay").addEventListener('click',this._hideModal.bind(this));
+    this.shadowRoot.querySelector('#clear_button').addEventListener('click', this._deleteButton.bind(this));
   }
   disconnectedCallback() {
     this.shadowRoot.querySelector("button").removeEventListener('click', this._showModal);
     this.shadowRoot.querySelector(".close").removeEventListener('click', this._hideModal);
     this.shadowRoot.querySelector(".overlay").removeEventListener('click',this._hideModal);
-    
   }
   _showModal() { 
     this._modalVisible = true;
     this._modal.style.display = 'block';
     this._overlay.style.display = 'block';
+    let count = localStorage.getItem("count")
+    count ++
+    this.shadowRoot.getElementById('count').innerHTML = count;
+    localStorage.setItem("count", count);
+    console.log(count);
+    if(count >= 5){
+      this._showButton();
+    }
    }
    _hideModal() {
      this._modalVisible = false;
      this._modal.style.display = 'none';
      this._overlay.style.display = 'none';
    }
+   _showButton() {
+    this._clear_buttonVisible = true;
+    this._clear_button.style.display = 'block';
+   }
+   _deleteButton(){
+     this._clear_buttonVisible = false;
+     this._clear_button.style.display = 'none';
+     localStorage.clear();
+     this._hideModal();
+    }
 
-
-   
-
-
-  
   attributeChangedCallback(name, oldValue, newValue) {
     this.shadowRoot.querySelector("img.responsive").src = this.getAttribute("avatar");
     this.shadowRoot.querySelector("h1").innerText = this.getAttribute("name");
