02bc525fab6c87df3f553b838eeba7d3eaf43347
Michal Jeznach <michal6h@wp.pl>
2022-06-08 11:17:44 +0200

    100% done

--- src/WebComponents/index.js
+++ src/WebComponents/index.js
@@ -1,8 +1,7 @@
-
-
 const template = document.createElement("template");
 template.innerHTML = `
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap');
 @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap');
 
 .container { 
@@ -27,7 +26,7 @@ position: absolute;
 width: 380px;
 height: 250px;
 left: 830px;
-top: 136px;
+top: 120px;
 }
 
 .title {
@@ -39,12 +38,11 @@ top: calc(50% - 50px/2 - 319px);
 }
 
 .title-text {
- 
 font-family:'Roboto';
 font-style: normal;
 font-weight: 700;
 font-size: 42px;
-line-height: 120%;
+line-height: 50px;
 color: #121212;
 }
 
@@ -86,17 +84,41 @@ color: #FFFFFF;
 }
 
 .modal {
+position:fixed;
 display: none;
 width:920px;
 height:210px;
-left:50%;
-top:50%;
-margin:-100px 0 0 -450px;
-position:fixed;
+left: 260px;
+top: 375px;
 border: 1px solid black;
 background: #FFFFFF;
 z-index: 3;
 }
+
+.info-modal{
+  position: absolute;
+  width: 380px;
+  height: 24px;
+  left: calc(50% - 380px/2 - 175px);
+  top: calc(50% - 24px/2 + 37px);
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: "blend";
+  font-size: 16px;
+  line-height: 150%;
+  }
+
+  .overlay {
+    background: rgba(0, 0, 0, 0.6);
+    position: fixed;
+    display: none;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 2;
+    }
+  
 .header {
   position: fixed;
   width: 380px;
@@ -105,6 +127,7 @@ z-index: 3;
   top: 431px;
   
 }
+
 .header-text {
 position: absolute;
 width: 380px;
@@ -117,6 +140,7 @@ font-weight: 700;
 font-size: 42px;
 line-height: 120%;
 }
+
 .close {
   position: absolute;
   Width: 12px;
@@ -125,30 +149,6 @@ line-height: 120%;
   right: 46px;
   font-size: 30px;
   color: black;
-}
-.overlay {
-background: rgba(0, 0, 0, 0.6);
-position: fixed;
-display: none;
-top: 0;
-left: 0;
-width: 100%;
-height: 100%;
-z-index: 2;
-
-
-}
-.info-modal{
-position: absolute;
-width: 380px;
-height: 24px;
-left: calc(50% - 380px/2 - 175px);
-top: calc(50% - 24px/2 + 37px);
-font-family: 'Roboto';
-font-style: normal;
-font-weight: "blend";
-font-size: 16px;
-line-height: 150%;
 }
 
 .counter-text {
@@ -201,64 +201,76 @@ color: black;
 class WebComponent extends HTMLElement {
   constructor() {
     super();
-    
+
     const shadowRoot = this.attachShadow({ mode: "open" });
     shadowRoot.appendChild(template.content);
-    
   }
 
   static get observedAttributes() {
     return ["avatar", "name", "text"];
   }
   connectedCallback() {
-    
     this._modal = this.shadowRoot.querySelector(".modal");
     this._overlay = this.shadowRoot.querySelector(".overlay");
-    this._clear_button = this.shadowRoot.querySelector('#clear_button');
-    this.shadowRoot.querySelector("button").addEventListener('click',this._showModal.bind(this));
-    this.shadowRoot.querySelector(".close").addEventListener('click',this._hideModal.bind(this));
-    this.shadowRoot.querySelector(".overlay").addEventListener('click',this._hideModal.bind(this));
-    this.shadowRoot.querySelector('#clear_button').addEventListener('click', this._deleteButton.bind(this));
+    this._clear_button = this.shadowRoot.querySelector("#clear_button");
+    this.shadowRoot
+      .querySelector("button")
+      .addEventListener("click", this._showModal.bind(this));
+    this.shadowRoot
+      .querySelector(".close")
+      .addEventListener("click", this._hideModal.bind(this));
+    this.shadowRoot
+      .querySelector(".overlay")
+      .addEventListener("click", this._hideModal.bind(this));
+    this.shadowRoot
+      .querySelector("#clear_button")
+      .addEventListener("click", this._deleteButton.bind(this));
   }
   disconnectedCallback() {
-    this.shadowRoot.querySelector("button").removeEventListener('click', this._showModal);
-    this.shadowRoot.querySelector(".close").removeEventListener('click', this._hideModal);
-    this.shadowRoot.querySelector(".overlay").removeEventListener('click',this._hideModal);
+    this.shadowRoot
+      .querySelector("button")
+      .removeEventListener("click", this._showModal);
+    this.shadowRoot
+      .querySelector(".close")
+      .removeEventListener("click", this._hideModal);
+    this.shadowRoot
+      .querySelector(".overlay")
+      .removeEventListener("click", this._hideModal);
   }
-  _showModal() { 
+  _showModal() {
     this._modalVisible = true;
-    this._modal.style.display = 'block';
-    this._overlay.style.display = 'block';
-    let count = localStorage.getItem("count")
-    count ++
-    this.shadowRoot.getElementById('count').innerHTML = count;
+    this._modal.style.display = "block";
+    this._overlay.style.display = "block";
+    let count = localStorage.getItem("count");
+    count++;
+    this.shadowRoot.getElementById("count").innerHTML = count;
     localStorage.setItem("count", count);
     console.log(count);
-    if(count >= 5){
+    if (count >= 5) {
       this._showButton();
     }
-   }
-   _hideModal() {
-     this._modalVisible = false;
-     this._modal.style.display = 'none';
-     this._overlay.style.display = 'none';
-   }
-   _showButton() {
+  }
+  _hideModal() {
+    this._modalVisible = false;
+    this._modal.style.display = "none";
+    this._overlay.style.display = "none";
+  }
+  _showButton() {
     this._clear_buttonVisible = true;
-    this._clear_button.style.display = 'block';
-   }
-   _deleteButton(){
-     this._clear_buttonVisible = false;
-     this._clear_button.style.display = 'none';
-     localStorage.clear();
-     this._hideModal();
-    }
+    this._clear_button.style.display = "block";
+  }
+  _deleteButton() {
+    this._clear_buttonVisible = false;
+    this._clear_button.style.display = "none";
+    localStorage.clear();
+    this._hideModal();
+  }
 
   attributeChangedCallback(name, oldValue, newValue) {
-    this.shadowRoot.querySelector("img.responsive").src = this.getAttribute("avatar");
+    this.shadowRoot.querySelector("img.responsive").src =
+      this.getAttribute("avatar");
     this.shadowRoot.querySelector("h1").innerText = this.getAttribute("name");
     this.shadowRoot.querySelector("p").innerText = this.getAttribute("text");
-    
   }
 }
 
